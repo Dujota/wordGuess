de07b5534c0ff3411a7d721f4047fa9ad3abc8b2
Denis <ddujota@internationalsafety.com>
2018-06-28 19:37:07 -0400

    crated ui elements and started working with the reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,48 @@
-const wins = document.getElementById("wins");
-const losses = document.getElementById("losses");
-const guessLeft = document.getElementById("guessLeft");
+// Game Variables
+
+let wins = 0;
+let losses = 0;
+let guessLeft = 10;
+let userGuesses = [];
+const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+// UI Functions
+const winsElement = () => {
+  document.getElementById('wins').innerHTML = wins;
+};
+
+const lossesElement = () => {
+  document.getElementById('losses').innerHTML = losses;
+};
+
+const guessLeftElement = () => {
+  document.getElementById('guessLeft').innerHTML = guessLeft;
+};
+
+const userGuessesElement = () => {
+  document.getElementById('userGuess').innerHTML = userGuesses;
+};
+
+gameChoice = () => {
+  randomChoice =
+    letters[Math.floor(Math.random() * Math.floor(letters.length))];
+};
+
+reset = () => {
+  guessLeft = 10;
+  userGuesses = [];
+  gameChoice();
+  winsElement();
+  lossesElement();
+};
+
+document.onkeyup = e => {
+  userChoice = e.key.toLowerCase();
+  console.log(userChoice);
+  guessLeft--;
+  if (gameChoice() === userChoice) {
+    wins++;
+  } else {
+    losses++;
+  }
+};
